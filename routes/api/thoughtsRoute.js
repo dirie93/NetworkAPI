8f462cfba15a3147e3ebc6968c593c943306db9d
dirie93 <mdirie19@gmail.com>
2023-08-21 18:12:37 +0100

    implemented user and thought routes

---
+++ routes/api/thoughtsRoute.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+
+const {
+  listThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  removeThought,
+  addReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtsController");
+
+// Retrieving all thoughts
+router.route("/").get(listThoughts).post(createThought);
+
+// Retrieve a thought by ID
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(removeThought);
+
+// Adding a reaction to a thought by ID
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// Deleting a reaction from a thought by thought ID and reaction ID
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
